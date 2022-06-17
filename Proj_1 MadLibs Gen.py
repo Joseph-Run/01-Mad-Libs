6c6edcf8ca639ae77a55050b2fcb5b05948398ff
Joseph Awurigwe <37212156+DasherCode@users.noreply.github.com>
2022-06-17 22:08:48 +0100

    Add files via upload

---
+++ Proj_1 MadLibs Gen.py
@@ -0,0 +1,15 @@
+#Mad Libs
+
+def madlibs1():
+    animal_1 = input("Name of an Animal: ")
+    animal_2 = input("Name of another Animal: ")
+    food = input("Name of a food: ")
+    ing_verb = input("Name of a verb that ends in ing: ")
+    adverb_1 = input("An adverb: ")
+    adverb_2 = input("Another adverb: ")
+    noun_thing = input("Name of a thing(Noun): ")
+    body_part = input("Part of the body: ")
+    
+    print(f"The {animal_1}, the {animal_2}, and the {food} \n A(n) {animal_1} was {ing_verb} {adverb_1} before an open {noun_thing} where some {food} were roasting. A(n) {animal_2} who was hungrily eyeing the {food} said to the {animal_1}, “Do you think you could pull the {food} out of the ?{noun_thing} Your {body_part} seem(s) to be made for that.” The {animal_1} was flattered, and {adverb_2} took out one that had just melted. “How do you manage to do it?” asked the {animal_2} “Can you reach that big one?” “Yes, here it is,” said the . {animal_1} “But I have ruined my {body_part} a little.” “Oh, but what is that when you are making yourself so useful,” replied the . {animal_2} One after the other, the {animal_1} pulled the {food} out of the {noun_thing} and then learned that the sly {animal_2} had eaten them all. The poor {animal_1} had no reward except a ruined {body_part}. ")
+
+madlibs1()
