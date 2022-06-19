8ab4347a3b56453331aedf4d61b5edf564f976fb
Joseph Awurigwe <37212156+DasherCode@users.noreply.github.com>
2022-06-19 16:27:50 +0100

    Update Proj_1 MadLibs Gen.py
    added Madlibs2

--- Proj_1 MadLibs Gen.py
+++ Proj_1 MadLibs Gen.py
@@ -13,3 +13,18 @@ def madlibs1():
     print(f"The {animal_1}, the {animal_2}, and the {food} \n A(n) {animal_1} was {ing_verb} {adverb_1} before an open {noun_thing} where some {food} were roasting. A(n) {animal_2} who was hungrily eyeing the {food} said to the {animal_1}, “Do you think you could pull the {food} out of the ?{noun_thing} Your {body_part} seem(s) to be made for that.” The {animal_1} was flattered, and {adverb_2} took out one that had just melted. “How do you manage to do it?” asked the {animal_2} “Can you reach that big one?” “Yes, here it is,” said the . {animal_1} “But I have ruined my {body_part} a little.” “Oh, but what is that when you are making yourself so useful,” replied the . {animal_2} One after the other, the {animal_1} pulled the {food} out of the {noun_thing} and then learned that the sly {animal_2} had eaten them all. The poor {animal_1} had no reward except a ruined {body_part}. ")
 
 madlibs1()
+
+def madlibs2():
+    c_noun_animal = input("Name of an Animal: ")
+    c_name_person = input("Name of a person: ")
+    ing_verb = input("Name of a verb that ends in ing: ")
+    adjective_1 = input("An adjective: ")
+    sense = input("One of the five senses: ")
+    body_part = input("Part of the body: ")
+    adjective_2 = input("Another adjective: ")
+    adjective_3 = input("Just one more adjective: ")
+    
+    print(f"The {c_noun_animal} and the {c_noun_person}")
+    print(f"A(n) {c_noun_animal} was {ing_verb} along a road leading down the mountain side, when the {c_noun_animal} suddenly took it into his {adjective_1} head to go a different route. He could {sense} his home at the foot of the mountain, and to him the quickest way down seemed to be over the edge of the nearest cliff. Just as the  {c_noun_animal} was about to leap over, the {c_noun_person} caught him by the {body_part} and tried to pull him back, but the {adjective_2} {c_noun_animal} would not yield and pulled with all his might. "Very well," said the {c_noun_person}, "Go your way, you {adjective_3} beast, and see where it leads you." With that the {c_noun_person} let go, and the {adjective_1} {c_noun_animal} tumbled {body_part} over heels down the mountain side.")
+    
+madlibs2()
